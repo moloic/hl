96c3acebbcdc4199d7dadb344543c9aa9add289f
moloic <moloic@foxmail.com>
2018-09-07 09:13:07 +0800

    orm 持久层

---
+++ Hl.dotNet.Orm/DataAccess/Attributes/PrimaryKeyAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hl.dotNet.Orm
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Interface)]
+    public class PrimaryKeyAttribute : Attribute
+    {
+        public PrimaryKeyAttribute()
+        {
+        }
+
+        public PrimaryKeyAttribute(string name)
+        {
+            _name = name;
+        }
+        private string _name; public virtual string Name { get { return _name; } set { _name = value; } }
+    }
+}
---
+++ Hl.dotNet.Orm/DataAccess/DbProvider/DatabaseType.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hl.dotNet.Orm
+{
+    public enum DatabaseType
+    {
+        /// <summary>
+        /// 数据库类型：Oracle
+        /// </summary>
+        Oracle,
+        /// <summary>
+        /// 数据库类型：SqlServer
+        /// </summary>
+        SqlServer,
+        /// <summary>
+        /// 数据库类型：Access
+        /// </summary>
+        Access,
+        /// <summary>
+        /// 数据库类型：MySql
+        /// </summary>
+        MySql,
+        /// <summary>
+        /// 数据库类型：SQLite
+        /// </summary>
+        SQLite
+    }
+}
