99194fcbfb85ec732e9ef0c96c9b47bcc2e462ca
moloic <moloic@foxmail.com>
2018-09-07 09:19:47 +0800

    数据仓储

---
+++ Hl.dotNet.Repository/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hl.dotNet.Repository
+{
+    public class Class1
+    {
+    }
+}
---
+++ Hl.dotNet.Repository/DataFactory.cs
@@ -0,0 +1,46 @@
+﻿using Hl.dotNet.Orm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hl.dotNet.Repository
+{
+    /// <summary>
+    /// 操作数据库工厂
+    /// </summary>
+    public class DataFactory
+    {
+        private static readonly Object locker = new Object();
+        private static Database db = null;
+        /// <summary>
+        /// 获取指定的数据库连接
+        /// </summary>
+        /// <param name="connString"></param>
+        /// <returns></returns>
+        public static IDatabase Database(string connString)
+        {
+            //在并发时，使用单一对象
+            if (db == null)
+            {
+                return db = new Database(connString);
+            }
+            else
+            {
+                lock (locker)
+                {
+                    return db;
+                }
+            }
+        }
+        /// <summary>
+        /// 获取指定的数据库连接
+        /// </summary>
+        /// <returns></returns>
+        public static IDatabase Database()
+        {
+            return Database("BaseDb");
+        }
+    }
+}
